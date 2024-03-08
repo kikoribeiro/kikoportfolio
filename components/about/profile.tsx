e3312c0aa823ffaef66bce45d78f0faa20ede1b9
kikoribeiro <kikovribeiro@gmail.com>
2024-03-08 12:01:40 +0000

    Created About.tsx

---
+++ components/about/profile.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Profile() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Profile</h1>
+    </div>
+  );
+}
